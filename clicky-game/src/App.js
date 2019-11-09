b7b3c5f58994766c48f474e035a33d2ef705e72a
Austin <austin.dober@gmail.com>
2019-11-08 19:51:43 -0500

    adding working clicky game

---
+++ clicky-game/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import GameContainer from './components/gameContainer';
+
+
+
+function App() {
+  return (
+    <div> 
+      <GameContainer />
+    </div>
+  )
+}
+
+
+export default App;
---
+++ clicky-game/src/components/renderImages.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+var styling = {
+    container:{
+        padding: '10px',
+        border: 'none',
+        textAlign: 'center'
+    },
+    image: {
+        padding: '10px',
+        border: '1px solid rgba(0, 0, 0, 0.25)',
+        width: '200px',
+        height: '200px',
+        maxHeight: '200px',
+        maxWidth: '200px',
+        backgroundColor: '#86A3c3',
+        
+    }
+}
+const ImageRender = (props) => { 
+    return(
+        <div style={styling.container}className="container col-xs-6 col-sm-6 col-md-4 col-lg-3 col-xl-3">
+            <img
+                style = {styling.image}
+                id = {props.name}
+                src={props.image}
+                alt={props.name}
+                key={props.id} onClick={()=>props.handleClick(props.id, props.clicked)}>  
+            </img>
+            
+        </div>
+    )   
+}
+
+export default ImageRender
